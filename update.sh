47f006f53a2c1116d504543a5bf01d26d5f6efee
Aronne Brivio <aroblu94@gmail.com>
2015-09-23 15:25:56 +0200

    v2.0
    Fixed adb connections
    Minor fixes

--- update.sh
+++ update.sh
@@ -19,12 +19,9 @@ show_sections_title() {
 
 upgrade() {
 	show_sections_title "Upgrade process started."
+	prepare_adb
 	# Clean old files if any
 	clean_tmp
-	# Start ADB as sudo
-	sudo adb start-server
-	# Forward
-	sudo adb forward tcp:6000 localfilesystem:/data/local/debugger-socket
 	# Download update files
 	echo -e "Downloading Gaia and B2G from Mozilla servers..."
 	wget http:$url/gaia.zip
@@ -45,37 +42,31 @@ upgrade() {
 	echo -e "### Done."	 
 	# Update the phone
 	echo -e "Updating Flame..."
-	if adb wait-for-device; then
-		adb root
-		echo -e "### adb restarted with root privileges"
-		adb shell stop b2g
-		echo -e "### b2g stopped."
-		adb remount
-		# Remount root partition read-write
-		adb shell mount -o rw,remount /
-		# Remount system partition read-write
-		adb shell mount -o rw,remount /system
-		echo -e "### Device remounted with rw privileges."
-		adb shell rm -r /system/b2g
-		echo -e "### /system/b2g wiped."
-		if adb push system/b2g /system/b2g; then
-			echo -e "### Pushed new version of b2g."
-			echo -e "Restarting b2g..."
-			adb shell start b2g
-			echo -e "### Done."
-			show_sections_title "Upgrade complete!"
-			loop
-		fi
+	adb shell stop b2g
+	echo -e "### b2g stopped."
+	adb remount
+	# Remount root partition read-write
+	adb shell mount -o rw,remount /
+	# Remount system partition read-write
+	adb shell mount -o rw,remount /system
+	echo -e "### Device remounted with rw privileges."
+	adb shell rm -r /system/b2g
+	echo -e "### /system/b2g wiped."
+	if adb push system/b2g /system/b2g; then
+		echo -e "### Pushed new version of b2g."
+		echo -e "Restarting b2g..."
+		adb shell start b2g
+		echo -e "### Done."
+		show_sections_title "Upgrade complete!"
+		quest
 	fi
 }
 
 upgrade_base() {
 	show_sections_title "Upgrading to the lastest base..."
+	prepare_adb
 	# Clean old files if any
 	clean_tmp
-	adb wait-for-device
-	adb root
-	echo -e "### adb restarted with root privileges"
 	wget http://cds.w5v8t3u9.hwcdn.net/$base.zip
 	unzip $base.zip
 	echo -e "### Lastest base downloaded and extracted."
@@ -86,7 +77,7 @@ upgrade_base() {
 		rm -r $base.zip $base/
 		echo -e "### Removed files."
 		show_sections_title "Base upgrade complete!"
-		loop
+		quest
 	fi
 }
 
@@ -94,22 +85,18 @@ backup() {
 	# Backup b2g
 	#backup=`adb pull /system/b2g backup/`
 	show_sections_title "Backing up /system/b2g into 'backup' folder (overwrite folder if already exists)..."
+	prepare_adb
 	rm -rf backup &> /dev/null
-	adb wait-for-device
-	adb root
-	echo -e "### adb restarted with root privileges"
 	if adb pull /system/b2g backup/; then
 		show_sections_title "Upgrade complete!"
-		loop
+		quest
 	fi
 }
 
 restore() {
 	# Restore b2g from backup/
 	show_sections_title "Restoring backup from backup/ to /system/b2g..."
-	adb wait-for-device
-	adb root
-	echo -e "### adb restarted with root privileges"
+	prepare_adb
 	adb shell stop b2g
 	echo -e "### b2g stopped."
 	adb remount
@@ -121,22 +108,21 @@ restore() {
 		adb shell start b2g
 		echo -e "### Done."
 		show_sections_title "Restore complete!"
-		loop
+		quest
 	fi
 }
 
 udev() {
 	show_sections_title "Adding an UDEV rule for Flame (if not present yet)..."
-	var=$(less /etc/udev/rules.d/android.rules | grep 05c6)
-	exit_status=$?
-	if [$exit_status -eq 0]; then
-		echo -e "### UDEV rule for Flame already present."
-		loop
-	else
-		sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="05c6", MODE="0666", GROUP="plugdev"' >> /etc/udev/rules.d/android.rules
-		echo -e "### UDEV rule for Flame added."
-		loop
-	fi
+	echo -e "ACTUALLY NOT WORKING. Exiting"
+	#myvar=$(less /etc/udev/rules.d/android.rules | grep 05c6)
+	#if [${#myvar} -eq 0]; then
+	#	sudo echo 'SUBSYSTEM=="usb", ATTR{idVendor}=="05c6", MODE="0666", GROUP="plugdev"' >> /etc/udev/rules.d/android.rules
+	#	echo -e "### UDEV rule for Flame added."
+	#else
+	#	echo -e "### UDEV rule for Flame already present."
+	#fi
+	quest
 }
 
 clean_tmp() {
@@ -151,11 +137,17 @@ clean_tmp() {
 
 prepare_adb() {
 	echo -e "Setting ADB"
-	adb start-service
+	sudo adb kill-server
+	# Start ADB as sudo
+	sudo adb start-server
 	echo -e "### adb service started"
-	adb devices
-	adb root
-	echo -e "### adb restarted with root privileges"
+	if adb wait-for-device; then
+		echo -e "### device found"
+		# Forward
+		sudo adb forward tcp:6000 localfilesystem:/data/local/debugger-socket
+		adb root
+		echo -e "### adb restarted with root privileges"
+	fi
 }
 
 end() {
@@ -196,6 +188,19 @@ loop() {
 		* ) 
 			echo -e "Sorry, try again." 
 			loop
+		;;
+	esac
+}
+
+quest() {
+	show_sections_title "Do you want to do other stuff? [y/n]"
+	read RES
+	case $RES in
+		[y]* )
+			loop
+		;;
+		[n]* )
+			end
 		;;
 	esac
 }
